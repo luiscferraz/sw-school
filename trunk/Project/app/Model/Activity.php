20593eec3497d20dbffd9ea7a14df3afbd9aaee6
palaciowagner <palaciowagner@32593ff9-090c-9659-684d-f84e3a8649d6>
2013-01-21 13:30:32 +0000

    Adição dos Models Activity e Pointing

---
+++ trunk/Project/app/Model/Activity.php
@@ -0,0 +1,14 @@
+<?php
+
+class Activity extends AppModel{
+	
+	public $name = 'Activity';
+	public $useTable = 'activities';
+	var $belongsTo = array('Project');   //Várias atividades pertencem a um projeto
+	public $hasMany = array('Pointing'); //Uma atividade pode ter vários apontamentos
+   public $hasAndBelongsToMany = array('Consultant'); //Uma atividade pode ser executada por vários
+   													  //consultores, e um consultor pode executar
+   													  //várias atividades
+    
+}
+?>
