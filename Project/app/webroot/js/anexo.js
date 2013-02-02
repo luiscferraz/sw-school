c8ede0394ef8b14b8f434a6a955eb82d91b9a4a1
palaciowagner <palaciowagner@32593ff9-090c-9659-684d-f84e3a8649d6>
2013-02-02 02:18:54 +0000

    Alterações nas ActivitiesController, AttachmentsController, Attachment, AjaxListFiles,Activities/index.ctp,anexo.js para criação da função de visualização de anexos.

--- Project/app/webroot/js/anexo.js
+++ Project/app/webroot/js/anexo.js
@@ -1,14 +1,69 @@
-function ListAttachments(key){
-	//$.get(urlAjax('AjaxListFiles'),null,
-	//		function(data) {
-				//alert(data)   ;
-				$.fancybox('erick');
-	//			$('.load').remove();
-	//	})
+$('document').ready(function(){
+	
+	$('.edit').live('click',function(e){
+		$(this).html(createInput($(this).parent('p').html()));
+		$('.edit p').show();
+		$('.edit textarea').hide();
+		$(this).children('p').hide();
+		$(this).children('textarea').show();
+		$(this).children('textarea').focus();
+	});
+	$('.edit textarea').keyup(function(){
+			var hr = $(this).val();
+			$(this).siblings('p').html(hr);
+		})
+
+
+})
+
+
+
+function createInput(html){
+	html ='<textarea> '+html+' </textarea>'
+	return html;
 }
 
-var input = $("input:file").css({background:"yellow", border:"3px red solid"});
-    $("div").text("For this type jQuery found " + input.length + ".")
-            .css("color", "red");
-    $("form").submit(function () { return false; }); // so it won't submit
+
+
+//gif para carregamento ajax
+$(document).ajaxStart(function() {
+	   $('body').append('<img src="'+limparUrl('img/loading.gif')+'" class="load">');
+});
+
+
+function limparUrl(pag){
+	var url = window.location.toString();
+	n =  url.search('attachments');
+	url = url.slice(0,n);
+	return url+'/'+pag;
+	
+}
+
+function ListAttachments(key){
+	$.get(urlAjax('AjaxListFiles'),null,
+			function(data) {
+				//alert(data)   ;
+				$.fancybox(data);
+				$('.load').remove();
+		})
+}
+
+//evitar erros de url ajax
+function urlAjax(pag){
+	var url = window.location.toString();
+	
+	if (url.search('add') != '-1'){
+		url = url.replace('add',pag);
+	}
+	else if(url.search('edit') != '-1'){
+		url = url.replace('edit',pag);
+	}
+	else {
+		url = url.replace('alocados',pag);
+	}
+	
+	return url;
+}
+
+
  
