ec76522c33ad4952bd4cf4b683e993bfd800668f
diasflaviano@gmail.com <diasflaviano@gmail.com@32593ff9-090c-9659-684d-f84e3a8649d6>
2013-02-04 13:34:32 +0000

    Link para a agenda

--- trunk/Project/app/View/Layouts/includes/menu.php
+++ trunk/Project/app/View/Layouts/includes/menu.php
@@ -2,6 +2,7 @@
 	<a href="#" id="botao_home"><?php echo $this->Html->image('logo.png', array('alt' => 'sw', 'id'=>'logoSw'))?>
 		<?php echo $this->Html->image('botao_home.png', array('alt' => '', 'id'=>'botao_home_img'))?></a>
 		<ul>
+			<li><?php echo $this->Html->link('Agenda', array('action' =>'../home'));?></li>
 			<li><?php echo $this->Html->link('Consultor', array('action' =>'../consultants'));?></li>
 			<li><?php echo $this->Html->link('Empresa', array('action' =>'../companies'));?></li>
 			<li><?php echo $this->Html->link('Projeto', array('action' =>'../projects'));?></li>
