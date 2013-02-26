ad042e36fbb04fbc9dfbe1ded71c97706464d692
rodrigofelixrodrigo@gmail.com <rodrigofelixrodrigo@gmail.com@32593ff9-090c-9659-684d-f84e3a8649d6>
2013-02-26 21:48:05 +0000

    Sprint 5 - Agenda - Ligação inicial com o banco

--- Project/app/Controller/HomeController.php
+++ Project/app/Controller/HomeController.php
@@ -11,5 +11,23 @@
                $this->layout =  'main';
 			   $this-> set ('tipo_usuario',$this->Auth->user('type'));	
         }
+		
+		public function atividades_agenda($id_projeto) {
+			$this->layout = 'atividades_agenda';
+			//procura a primeira atividade que achar para o projeto escolhido
+			$activities = $this->Home->Activity->find('first',array('conditions'=> array('Activity.removed !=' => 1, 'Activity.project_id =' => $id_projeto)));						
+			//cria uma lista que vai conter tuplas, Ex: (descricao:escola,data:01-01-2000,horas:9)
+			$atividade = array(); 	
+			//se nao achar nada descricao vai em branco
+			if ($activities == null){
+			$atividade['descricao'] = '';
+			}
+			else{
+			//definindo uma tupla descricao dentro do array ativdade para ser a descricao da primeira atividade que ele encontrou
+			$atividade['descricao'] = $activities['Activity']['description'];		
+			}
+			//retorna o array para o ajax
+			echo json_encode($atividade);		
+		}		
  }
 ?>
