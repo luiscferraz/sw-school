1f9cd38c21b05eda26ceca8ff2a70c709b66ac32
diasflaviano@gmail.com <diasflaviano@gmail.com@32593ff9-090c-9659-684d-f84e3a8649d6>
2013-03-04 14:36:28 +0000

    Sprint 5 - Agenda/Relatorios/Menu - busca da url, inclusao do botao, nomes no plural.

--- trunk/Project/app/View/Layouts/includes/menu.php
+++ trunk/Project/app/View/Layouts/includes/menu.php
@@ -3,11 +3,11 @@
 		<?php echo $this->Html->image('botao_home.png', array('alt' => '', 'id'=>'botao_home_img'))?></a>
 		<ul>
 			<li><?php echo $this->Html->link('Agenda', array('action' =>'../home'));?></li>
-			<li><?php echo $this->Html->link('Consultor', array('action' =>'../consultants'));?></li>
-			<li><?php echo $this->Html->link('Empresa', array('action' =>'../companies'));?></li>
-			<li><?php echo $this->Html->link('Projeto', array('action' =>'../projects'));?></li>
-			<li><?php echo $this->Html->link('Atividade', array('action' =>'../activities'));?></li>
-			<li><?php echo $this->Html->link('Apontamento', array('action' =>'../entries'));?></li>
+			<li><?php echo $this->Html->link('Consultores', array('action' =>'../consultants'));?></li>
+			<li><?php echo $this->Html->link('Empresas', array('action' =>'../companies'));?></li>
+			<li><?php echo $this->Html->link('Projetos', array('action' =>'../projects'));?></li>
+			<li><?php echo $this->Html->link('Atividades', array('action' =>'../activities'));?></li>
+			<li><?php echo $this->Html->link('Apontamentos', array('action' =>'../entries'));?></li>
 			<li><?php echo $this->Html->link('Relatórios', array('action' =>'../reports'));?></li>
 			<li><?php echo $this->Html->link('Sair', array('action' =>'../users/logout'));?></li>
 		</ul>
