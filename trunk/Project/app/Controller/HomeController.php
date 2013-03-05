7c62c4c80c54fed22f677e678348e2044b30be42
nandacms@gmail.com <nandacms@gmail.com@32593ff9-090c-9659-684d-f84e3a8649d6>
2013-03-05 00:53:02 +0000

    Sprint 5 - Agenda  - Apontamento parcial das atividades de um projeto na agenda.

--- trunk/Project/app/Controller/HomeController.php
+++ trunk/Project/app/Controller/HomeController.php
@@ -12,23 +12,34 @@
 			   $this-> set ('tipo_usuario',$this->Auth->user('type'));	
 			   $this-> set ('projects',$this->Home->Project->find('all', array('conditions'=> array('Project.removed !=' => 1))));
         }
-		
+     
 		public function atividades_agenda($id_projeto) {
 			$this->layout = 'atividades_agenda';
 			//procura a primeira atividade que achar para o projeto escolhido
-			$activities = $this->Home->Activity->find('first',array('conditions'=> array('Activity.removed !=' => 1, 'Activity.project_id =' => $id_projeto)));						
+			$activities = $this->Home->Activity->find('all',array('conditions'=> array('Activity.removed !=' => 1, 'Activity.project_id =' => $id_projeto)));						
 			//cria uma lista que vai conter tuplas, Ex: (descricao:escola,data:01-01-2000,horas:9)
-			$atividade = array(); 	
-			//se nao achar nada descricao vai em branco
-			if ($activities == null){
-			$atividade['descricao'] = '';
+			$atividade = array(); 
+			
+			foreach ($activities as $activity){
+				$atividade[] = $activity['Activity']['id'];	
 			}
-			else{
+			//retorna os ids de todas as atividades de determinado projeto
+			//print_r ($atividade);
+			echo json_encode($atividade);
+		}
+			
+		public function mostrar_atividade($id_atividade){
+			$this->layout = 'atividades_agenda';
+			$atividade = array();
+			$activity = $this->Home->Activity->find('first',array('conditions'=> array('Activity.removed !=' => 1, 'Activity.id =' => $id_atividade)));						
+			
 			//definindo uma tupla descricao dentro do array ativdade para ser a descricao da primeira atividade que ele encontrou
-			$atividade['descricao'] = $activities['Activity']['description'];		
-			}
+			$atividade['descricao'] = $activity['Activity']['description'];
+			$atividade['data'] = $activity['Activity']['date'];
+			
+			
 			//retorna o array para o ajax
-			echo json_encode($atividade);		
+			echo json_encode($atividade);
 		}		
  }
 ?>
