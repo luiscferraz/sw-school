87dbd6c6a1223f823fa2302b5f8222866e915353
rodrigofelixrodrigo@gmail.com <rodrigofelixrodrigo@gmail.com@32593ff9-090c-9659-684d-f84e3a8649d6>
2013-03-07 02:06:17 +0000

    Sprint 6 - Agenda - Atualização dos inputs

--- trunk/Project/app/webroot/js/agenda.js
+++ trunk/Project/app/webroot/js/agenda.js
@@ -1,6 +1,6 @@
 $('document').ready(function(){
 
-$('table td').click(function(){
+$('table td').dblclick(function(){
 $('table td input').remove();
         $(this).html('<input type="text" style="width: 12px;">')});
 		
