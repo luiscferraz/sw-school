ca96cb5b482e992b2cb40ef52c7cd1e9c7a89841
diasflaviano@gmail.com <diasflaviano@gmail.com@32593ff9-090c-9659-684d-f84e3a8649d6>
2013-03-28 03:47:59 +0000

    Sprint 8 - Ajustes e correções - email e username como unicos

--- Project/app/Model/User.php
+++ Project/app/Model/User.php
@@ -1,13 +1,20 @@
 <?php
 class User extends AppModel {
     public $name = 'User';
+    public $validate = array(
+    'username' => array(
+        'rule'    => 'isUnique',
+        'message' => 'Usuário ja Cadastrado'
+    	)
+    );
 	
 	public function beforeSave($options = array()) {
 		if (isset($this->data[$this->alias]['password'])) {
 			$this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
 		}
 		return true;
-}
+	}
+
 }
 
 ?>
