899b63db7bb220a9cc003e731080a1dc402a4a07
felixflneto@yahoo.com.br <felixflneto@yahoo.com.br@32593ff9-090c-9659-684d-f84e3a8649d6>
2013-07-30 02:26:51 +0000

    Sprint 6 - Validar hora nos inputs de Add/Edit em Atividades

--- src/Project/app/Model/Activity.php
+++ src/Project/app/Model/Activity.php
@@ -1,14 +1,18 @@
 <?php
 
-class Activity extends AppModel{
-	
-	public $name = 'Activity';
-	public $useTable = 'activities';
-	var $belongsTo = array('Project');   //Várias atividades pertencem a um projeto
-	public $hasMany = array('Entry','Attachment'); //Uma atividade pode ter vários apontamentos e anexos
-   	public $hasAndBelongsToMany = array('Consultant'); //Uma atividade pode ser executada por vários
-   													  //consultores, e um consultor pode executar
-   													  //várias atividades
-    
-}
+	class Activity extends AppModel{
+		
+		public $name = 'Activity';
+		public $useTable = 'activities';
+		var $belongsTo = array('Project');   //VÃ¡rias atividades pertencem a um projeto
+		public $hasMany = array('Entry','Attachment'); //Uma atividade pode ter vÃ¡rios apontamentos e anexos
+	   	public $hasAndBelongsToMany = array('Consultant'); //Uma atividade pode ser executada por vÃ¡rios
+	   													  //consultores, e um consultor pode executar
+	   		
+	   													  //vÃ¡rias atividades
+	   	public $validate = array(
+	    	'start_hours' => '/^([0-1][0-9]|[2][0-3]):([0-5][0-9])$/i',
+	    	'end_hours' => '/^([0-1][0-9]|[2][0-3]):([0-5][0-9])$/i'	
+		);
+	}
 ?>
