f865a7cb9bcc7f5a4cd45c2d2751d681d4f7c1df
felixflneto@yahoo.com.br <felixflneto@yahoo.com.br@32593ff9-090c-9659-684d-f84e3a8649d6>
2013-08-03 19:34:29 +0000

    Atividades - Removendo Função de validação de hora, do Model(PHP), para fazer em javascript

--- trunk/src/Project/app/Model/Activity.php
+++ trunk/src/Project/app/Model/Activity.php
@@ -8,11 +8,13 @@
 		public $hasMany = array('Entry','Attachment'); //Uma atividade pode ter vários apontamentos e anexos
 	   	public $hasAndBelongsToMany = array('Consultant'); //Uma atividade pode ser executada por vários
 	   													  //consultores, e um consultor pode executar
-	   		
-	   													  //várias atividades
-	   	public $validate = array(
-	    	'start_hours' => '/^([0-1][0-9]|[2][0-3]):([0-5][0-9])$/i',
-	    	'end_hours' => '/^([0-1][0-9]|[2][0-3]):([0-5][0-9])$/i'	
-		);
+	   													 //várias atividades
+
+	   	// Função que valida as horas inicial e final (Foi criada função em javascript)
+//	   	public $validate = array(
+//	    	'start_hours' => '/^([0-1][0-9]|[2][0-3]):([0-5][0-9])$/i',
+//	    	'end_hours' => '/^([0-1][0-9]|[2][0-3]):([0-5][0-9])$/i'
+//		);
+
 	}
 ?>
